a330e413f0b0f1e1c069ac705daa33398888ccde
gfo974 <geoffrey@arribada.org>
2025-10-15 13:42:04 +0400

    merge last change with makefile and set PB3 default pin for Wakeup from Standby

--- build_info.c
+++ build_info.c
@@ -1,3 +1,3 @@
 #include "build_info.h"
 
-const char uc_fw_vers_commit_id[] = "5ad8cd5*_TxSlp_gui_blind_Mp,v10.0.0_c707759_0x6Tx,v9.5.2_c707759_Tx,Oct 14 2025_19:13:30";
+const char uc_fw_vers_commit_id[] = "1d2c415*_DVTxStdb_gui_blind_Mp,v10.0.0_c707759_0x6Tx,v9.5.2_c707759_Tx,Oct 15 2025_13:32:57";
