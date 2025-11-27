2831c7b25ad079184ec901b1853daf03bc8bd153
gfo974 <geoffrey@arribada.org>
2025-11-27 12:29:43 +0400

    Fix 1200 error when trying to set ID/ADDR ..., Add flash retry and waitReady

--- build_info.c
+++ build_info.c
@@ -1,3 +1,3 @@
 #include "build_info.h"
 
-const char uc_fw_vers_commit_id[] = "1d2c415*_DVTxStdb_gui_blind_Mp,v10.0.0_c707759_0x6Tx,v9.5.2_c707759_Tx,Oct 15 2025_13:32:57";
+const char uc_fw_vers_commit_id[] = "a330e41*_DVTx_gui_basic_Mp,v10.0.0_c707759_0x6Tx,v9.5.2_c707759_Tx,Nov 27 2025_12:23:44";
